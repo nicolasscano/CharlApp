620347c86abad73f9224efa1795bc2636c797d47
nicolasscano <ncdecelis@gmail.com>
2024-01-24 14:40:26 +0100

    Más avances

--- app/src/main/java/com/canodevs/charlapp/Chat/MensajesActivity.kt
+++ app/src/main/java/com/canodevs/charlapp/Chat/MensajesActivity.kt
@@ -10,6 +10,8 @@ import android.widget.Toast
 import com.bumptech.glide.Glide
 import com.canodevs.charlapp.Modelo.Usuario
 import com.canodevs.charlapp.R
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
 import com.google.firebase.database.DataSnapshot
 import com.google.firebase.database.DatabaseError
 import com.google.firebase.database.FirebaseDatabase
@@ -22,6 +24,7 @@ class MensajesActivity : AppCompatActivity() {
     private lateinit var Et_mensaje: EditText
     private lateinit var IB_Enviar: ImageButton
     var uid_usuario_seleccionado: String = ""
+    var firebaseUser : FirebaseUser ?= null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -35,7 +38,8 @@ class MensajesActivity : AppCompatActivity() {
             if (mensaje.isEmpty()) {
                 Toast.makeText(applicationContext, "Escribe un mensaje", Toast.LENGTH_SHORT).show()
             } else {
-                enviarMensaje()
+                enviarMensaje(firebaseUser!!.uid, uid_usuario_seleccionado, mensaje)
+                Et_mensaje.setText("")
             }
         }
     }
@@ -45,7 +49,43 @@ class MensajesActivity : AppCompatActivity() {
         uid_usuario_seleccionado = intent.getStringExtra("uid_usuario").toString()
     }
 
-    private fun enviarMensaje() {
+    private fun enviarMensaje(uid_emisor : String, uid_receptor : String, mensaje : String) {
+        val reference = FirebaseDatabase.getInstance().reference
+        val mensajeKey = reference.push().key
+
+        val infoMensaje = HashMap<String, Any?> ()
+        infoMensaje["id_mensaje"] = mensajeKey
+        infoMensaje["emisor"] = uid_emisor
+        infoMensaje["receptor"] = uid_receptor
+        infoMensaje["mensaje"] = mensaje
+        infoMensaje["url"] = ""
+        infoMensaje["visto"] = false
+        reference.child("Chats").child(mensajeKey!!).setValue(infoMensaje).addOnCompleteListener {tarea ->
+            if (tarea.isSuccessful) {
+                val listaMensajesEmisor =  FirebaseDatabase.getInstance().reference.child("ListaMensajes")
+                    .child(firebaseUser!!.uid)
+                    .child(uid_usuario_seleccionado)
+
+                listaMensajesEmisor.addListenerForSingleValueEvent(object : ValueEventListener {
+                    override fun onDataChange(snapshot: DataSnapshot) {
+                        if (!snapshot.exists()){
+                            listaMensajesEmisor.child("uid").setValue(uid_usuario_seleccionado)
+                        }
+
+                        val listaMensajesReceptor = FirebaseDatabase.getInstance().reference.child("ListaMensajes")
+                            .child(uid_usuario_seleccionado)
+                            .child(firebaseUser!!.uid)
+                        listaMensajesReceptor.child("uid").setValue(firebaseUser!!.uid)
+                    }
+
+                    override fun onCancelled(error: DatabaseError) {
+                        TODO("Not yet implemented")
+                    }
+
+                })
+            }
+
+        }
 
     }
 
@@ -54,6 +94,7 @@ class MensajesActivity : AppCompatActivity() {
         N_usuario_chat = findViewById(R.id.N_usuario_chat)
         Et_mensaje = findViewById(R.id.Et_mensaje)
         IB_Enviar = findViewById(R.id.IB_Enviar)
+        firebaseUser = FirebaseAuth.getInstance().currentUser
     }
 
     private fun leerInfoUsuarioSeleccionado() {
