3b68e679395608e9dbd765c8a8a5f87330bb567b
nicolasscano <ncdecelis@gmail.com>
2024-01-30 10:40:42 +0100

    Avances chats

--- app/src/main/java/com/canodevs/charlapp/Chat/MensajesActivity.kt
+++ app/src/main/java/com/canodevs/charlapp/Chat/MensajesActivity.kt
@@ -1,5 +1,8 @@
 package com.canodevs.charlapp.Chat
 
+import android.app.ProgressDialog
+import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.EditText
@@ -7,24 +10,34 @@ import android.widget.ImageButton
 import android.widget.ImageView
 import android.widget.TextView
 import android.widget.Toast
+import androidx.activity.result.ActivityResult
+import androidx.activity.result.ActivityResultCallback
+import androidx.activity.result.contract.ActivityResultContracts
 import com.bumptech.glide.Glide
 import com.canodevs.charlapp.Modelo.Usuario
 import com.canodevs.charlapp.R
+import com.google.android.gms.tasks.Continuation
+import com.google.android.gms.tasks.Task
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.FirebaseUser
 import com.google.firebase.database.DataSnapshot
 import com.google.firebase.database.DatabaseError
 import com.google.firebase.database.FirebaseDatabase
 import com.google.firebase.database.ValueEventListener
+import com.google.firebase.storage.FirebaseStorage
+import com.google.firebase.storage.StorageTask
+import com.google.firebase.storage.UploadTask
 
 class MensajesActivity : AppCompatActivity() {
 
     private lateinit var imagen_perfil_chat: ImageView
     private lateinit var N_usuario_chat: TextView
     private lateinit var Et_mensaje: EditText
+    private lateinit var IB_Adjuntar: ImageButton
     private lateinit var IB_Enviar: ImageButton
     var uid_usuario_seleccionado: String = ""
-    var firebaseUser : FirebaseUser ?= null
+    var firebaseUser: FirebaseUser? = null
+    private var imagenUri : Uri?= null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -42,6 +55,10 @@ class MensajesActivity : AppCompatActivity() {
                 Et_mensaje.setText("")
             }
         }
+
+        IB_Adjuntar.setOnClickListener {
+            abrirGaleria()
+        }
     }
 
     private fun obtenerUid() {
@@ -49,43 +66,46 @@ class MensajesActivity : AppCompatActivity() {
         uid_usuario_seleccionado = intent.getStringExtra("uid_usuario").toString()
     }
 
-    private fun enviarMensaje(uid_emisor : String, uid_receptor : String, mensaje : String) {
+    private fun enviarMensaje(uid_emisor: String, uid_receptor: String, mensaje: String) {
         val reference = FirebaseDatabase.getInstance().reference
         val mensajeKey = reference.push().key
 
-        val infoMensaje = HashMap<String, Any?> ()
+        val infoMensaje = HashMap<String, Any?>()
         infoMensaje["id_mensaje"] = mensajeKey
         infoMensaje["emisor"] = uid_emisor
         infoMensaje["receptor"] = uid_receptor
         infoMensaje["mensaje"] = mensaje
         infoMensaje["url"] = ""
         infoMensaje["visto"] = false
-        reference.child("Chats").child(mensajeKey!!).setValue(infoMensaje).addOnCompleteListener {tarea ->
-            if (tarea.isSuccessful) {
-                val listaMensajesEmisor =  FirebaseDatabase.getInstance().reference.child("ListaMensajes")
-                    .child(firebaseUser!!.uid)
-                    .child(uid_usuario_seleccionado)
+        reference.child("Chats").child(mensajeKey!!).setValue(infoMensaje)
+            .addOnCompleteListener { tarea ->
+                if (tarea.isSuccessful) {
+                    val listaMensajesEmisor =
+                        FirebaseDatabase.getInstance().reference.child("ListaMensajes")
+                            .child(firebaseUser!!.uid)
+                            .child(uid_usuario_seleccionado)
 
-                listaMensajesEmisor.addListenerForSingleValueEvent(object : ValueEventListener {
-                    override fun onDataChange(snapshot: DataSnapshot) {
-                        if (!snapshot.exists()){
-                            listaMensajesEmisor.child("uid").setValue(uid_usuario_seleccionado)
+                    listaMensajesEmisor.addListenerForSingleValueEvent(object : ValueEventListener {
+                        override fun onDataChange(snapshot: DataSnapshot) {
+                            if (!snapshot.exists()) {
+                                listaMensajesEmisor.child("uid").setValue(uid_usuario_seleccionado)
+                            }
+
+                            val listaMensajesReceptor =
+                                FirebaseDatabase.getInstance().reference.child("ListaMensajes")
+                                    .child(uid_usuario_seleccionado)
+                                    .child(firebaseUser!!.uid)
+                            listaMensajesReceptor.child("uid").setValue(firebaseUser!!.uid)
                         }
 
-                        val listaMensajesReceptor = FirebaseDatabase.getInstance().reference.child("ListaMensajes")
-                            .child(uid_usuario_seleccionado)
-                            .child(firebaseUser!!.uid)
-                        listaMensajesReceptor.child("uid").setValue(firebaseUser!!.uid)
-                    }
+                        override fun onCancelled(error: DatabaseError) {
+                            TODO("Not yet implemented")
+                        }
 
-                    override fun onCancelled(error: DatabaseError) {
-                        TODO("Not yet implemented")
-                    }
+                    })
+                }
 
-                })
             }
-
-        }
 
     }
 
@@ -93,6 +113,7 @@ class MensajesActivity : AppCompatActivity() {
         imagen_perfil_chat = findViewById(R.id.imagen_perfil_chat)
         N_usuario_chat = findViewById(R.id.N_usuario_chat)
         Et_mensaje = findViewById(R.id.Et_mensaje)
+        IB_Adjuntar = findViewById(R.id.IB_Adjuntar)
         IB_Enviar = findViewById(R.id.IB_Enviar)
         firebaseUser = FirebaseAuth.getInstance().currentUser
     }
@@ -119,5 +140,94 @@ class MensajesActivity : AppCompatActivity() {
         })
     }
 
+    private fun abrirGaleria() {
+        val intent = Intent(Intent.ACTION_PICK)
+        intent.type = "image/*"
+        galeriaARL.launch(intent)
+    }
+
+    private val galeriaARL = registerForActivityResult(
+        ActivityResultContracts.StartActivityForResult(),
+        ActivityResultCallback <ActivityResult> {resultado ->
+            if (resultado.resultCode == RESULT_OK) {
+                val data = resultado.data
+                imagenUri = data!!.data
+
+                val cargandoImagen = ProgressDialog(this@MensajesActivity)
+                cargandoImagen.setMessage("Enviando imagen...")
+                cargandoImagen.setCanceledOnTouchOutside(false)
+                cargandoImagen.show()
+
+                val carpetaImagenes = FirebaseStorage.getInstance().reference.child("Imágenes de mensajes")
+                val reference = FirebaseDatabase.getInstance().reference
+                val idMensaje = reference.push().key
+                val nombreImagen = carpetaImagenes.child("$idMensaje.jpg")
+
+                val uploadTask: StorageTask<*>
+                uploadTask = nombreImagen.putFile(imagenUri!!)
+                uploadTask.continueWithTask (Continuation <UploadTask.TaskSnapshot, Task<Uri>>{ task->
+                    if (!task.isSuccessful) {
+                        task.exception?.let {
+                            throw it
+                        }
+                    }
+                    return@Continuation nombreImagen.downloadUrl
+
+                }).addOnCompleteListener {task->
+                    if (task.isSuccessful) {
+                        cargandoImagen.dismiss()
+                        val downloadUrl = task.result
+                        val url = downloadUrl.toString()
+
+                        val infoMensajeImagen = HashMap<String, Any?>()
+                        infoMensajeImagen["id_mensaje"] = idMensaje
+                        infoMensajeImagen["emisor"] = firebaseUser!!.uid
+                        infoMensajeImagen["receptor"] = uid_usuario_seleccionado
+                        infoMensajeImagen["mensaje"] = "Imagen enviada"
+                        infoMensajeImagen["url"] = url
+                        infoMensajeImagen["visto"] = false
+
+                        reference.child("Chats").child(idMensaje!!).setValue(infoMensajeImagen)
+                            .addOnCompleteListener { tarea ->
+                                if (tarea.isSuccessful) {
+                                    val listaMensajesEmisor =
+                                        FirebaseDatabase.getInstance().reference.child("ListaMensajes")
+                                            .child(firebaseUser!!.uid)
+                                            .child(uid_usuario_seleccionado)
+
+                                    listaMensajesEmisor.addListenerForSingleValueEvent(object : ValueEventListener {
+                                        override fun onDataChange(snapshot: DataSnapshot) {
+                                            if (!snapshot.exists()) {
+                                                listaMensajesEmisor.child("uid").setValue(uid_usuario_seleccionado)
+                                            }
+
+                                            val listaMensajesReceptor =
+                                                FirebaseDatabase.getInstance().reference.child("ListaMensajes")
+                                                    .child(uid_usuario_seleccionado)
+                                                    .child(firebaseUser!!.uid)
+                                            listaMensajesReceptor.child("uid").setValue(firebaseUser!!.uid)
+                                        }
+
+                                        override fun onCancelled(error: DatabaseError) {
+                                            TODO("Not yet implemented")
+                                        }
+
+                                    })
+                                }
+
+                            }
+                        Toast.makeText(applicationContext, "Imagen enviada correctamente", Toast.LENGTH_SHORT).show()
+
+                    }
+
+                }
+
+            }
+            else {
+                Toast.makeText(applicationContext, "Envío cancelado por el usuario", Toast.LENGTH_SHORT).show()
+            }
+
+        }
+    )
 
 }
